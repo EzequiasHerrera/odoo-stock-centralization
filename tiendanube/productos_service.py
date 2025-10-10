225141c17eadd5ff772b46f82e5bf9f57de48315
Ezequias <ezequiasherrera99@gmail.com>
2025-10-10 17:57:22 -0400

    Implementing the integration between odoo and tn

--- tiendanube/productos_service.py
+++ tiendanube/productos_service.py
@@ -1,26 +1,78 @@
 import os
 import requests
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
+API_URL = os.getenv("TIENDANUBE_URL");
 STORE_ID = os.getenv("TIENDANUBE_TESTSTORE_ID")
 TOKEN = os.getenv("TIENDANUBE_ACCESS_TOKEN_TEST")
 SKU = "Test-ProB-Blan-S"
 
-url = f"https://api.tiendanube.com/v1/{STORE_ID}/products?q={SKU}"
+def buscar_sku_en_tienda_nube(sku):
+    url = f"{API_URL}/{STORE_ID}/products?q={sku}"
 
-headers = {
-    "Authentication": f"bearer {TOKEN}",
-    "User-Agent": "Test Odoo (ezequiasherrera99@gmail.com)",
-    "Content-Type": "application/json"
-}
+    headers = {
+        "Authentication": f"bearer {TOKEN}",
+        "User-Agent": "Test Odoo (ezequiasherrera99@gmail.com)",
+        "Content-Type": "application/json"
+    }
 
-response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-if response.status_code == 200:
+    if response.status_code != 200:
+        print(f"❌ Error {response.status_code}: {response.text}")
+        return None
+
     productos = response.json()
-    print(f"{productos}")
-else:
-    print(f"❌ Error {response.status_code}: {response.text}")
 
+    if not productos:
+        print("❌ No se encontró ningún producto con ese SKU.")
+        return None
+
+    producto = productos[0]  # Tomamos el primero que coincide
+    id_padre = producto["id"];
+
+    for variante in producto["variants"]:
+        if variante["sku"] == sku:
+            datos = {
+                "id_padre": id_padre,
+                "id": variante["id"],
+                "sku": variante["sku"],
+                "price": variante["price"],
+                "stock": variante["inventory_levels"][0]["stock"],
+                "values": [v["es"] for v in variante["values"]],
+                "producto_id": producto["id"],
+                "nombre": producto["name"]["es"],
+                "url": producto["canonical_url"]
+            }
+            return datos
+
+    print("❌ No se encontró ninguna variante con ese SKU exacto.")
+    return None
+
+def actualizar_stock_segun_sku(sku, stock):
+    product = buscar_sku_en_tienda_nube(sku);
+    id_padre = product["id_padre"]
+    id = product["id"]
+    
+    url = f"{API_URL}/{STORE_ID}/products/{id_padre}/variants/stock"
+    headers = {
+        "Authentication": f"bearer {TOKEN}",
+        "User-Agent": "Test Odoo (ezequiasherrera99@gmail.com)",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "action": "replace",
+        "value": stock,
+        "id": id
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    if response.status_code == 200:
+        print(f"✅ Stock actualizado a {stock} para producto {product['nombre']}")
+    else:
+        print(f"❌ Error al actualizar stock: {response.status_code} - {response.text}")
